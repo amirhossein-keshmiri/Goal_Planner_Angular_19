74cb254fd7c676c22d18fc0610315b2a7fc673c4
amirhossein keshmiri <amirhosseinky.dev@gmail.com>
2025-03-30 03:04:34 +0330

    Initial Project: - Integrate Bootstrap, Font Awesome, and ngx-toastr into the project. - Update Angular animations version to 19.2.4. - Configure styles and scripts in angular.json, and set up toastr in app.config.ts for notifications.

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,8 +1,18 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
-
+import { provideAnimations } from '@angular/platform-browser/animations';
+import { provideToastr, ToastrModule } from 'ngx-toastr';
 import { routes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
-  providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes)]
+  providers: [provideZoneChangeDetection({ eventCoalescing: true }),
+    provideRouter(routes),
+    provideAnimations(), // Required for animations
+    provideToastr({
+      timeOut: 3000,
+      positionClass: 'toast-top-right',
+      preventDuplicates: true,
+      progressBar: true,
+      closeButton: true,
+    }),]
 };
